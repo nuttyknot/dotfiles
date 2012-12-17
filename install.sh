60964f7d4284e57ef797cf75abda7ba31f9d13bf
Tiratat Patana-anake <nutty.knot@gmail.com>
2012-12-17 12:42:25 -0600

    Modify install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 
 wget --no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
 
-ln -s zsh/.zshrc ~/.zshrc
-ln -s zsh/knot.zsh-theme ~/.oh-my-zsh/themes/knot.zsh-theme
+PWD=`pwd`
+
+ln -s $PWD/zsh/.zshrc ~/.zshrc
+ln -s $PWD/zsh/knot.zsh-theme ~/.oh-my-zsh/themes/knot.zsh-theme
 
